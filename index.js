fd6ab6ae959abac98d0e6682e0057e2d4c48c89c
Cristina Lynn <cristina@cristinalynn.net>
2023-01-19 10:40:34 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(array, string) {
+    // console.log(array)
+    // console.log(string)
+   return array.filter(function (driverName) {
+        return string.toLowerCase() === driverName.toLowerCase()
+
+    });
+
+}
+
+function fuzzyMatch(array, string) {
+    // console.log(array)
+    // console.log(string)
+    return array.filter(function (driverName) {
+        return driverName.slice(0, 2) === string
+    })
+}
+
+function matchName(driver, string) {
+    // console.log(driver)
+    // console.log(string)
+    return driver.filter(function (driverObject) {
+        return driverObject["name"] === string
+    })
+
+}
